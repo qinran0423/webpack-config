895a258c5a45c3e821de701c42a336dd87995cc2
randy0423 <1156107187@qq.com>
2020-11-03 07:49:39 +0800

    多页面打包

--- webpack.mpa.config.js
+++ webpack.mpa.config.js
@@ -1,14 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const glob = require('glob');
+
+// 暗号：等价交换，炼金术不变的原则
+const setupMpa = () => {
+  const entry = {},
+    htmlPlugins = []
+  const entryPaths = glob.sync(path.join(__dirname, './src/*/index.js'))
+  entryPaths.forEach((item, index) => {
+    const path = item;
+    const match = path.match(/src\/(.*)\/index\.js$/);
+    const pageName = match[1];
+    entry[pageName] = path;
+    htmlPlugins.push(
+      new HtmlWebpackPlugin({
+        template: `./src/${pageName}/index.html`,
+        filename: `${pageName}.html`,
+        chunks: [pageName]
+      })
+    )
+  })
+  return {
+    entry,
+    htmlPlugins
+  }
+}
+
+const {entry, htmlPlugins} = setupMpa()
+
 module.exports = {
-  entry: './src/index.js',
+  entry,
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: '[name].js'
-  },
-  resolveLoader: {
-    modules: ["node_modules", "./myloaders"],
   },
   module: {
     rules: [
@@ -61,9 +86,7 @@ module.exports = {
     }
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+    ...htmlPlugins,
     new CleanWebpackPlugin()
   ]
 };
